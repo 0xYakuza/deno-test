dc585bd2d3d512b53a25504a82448e296d9340bb
MahdiA <41341335+AzerothA@users.noreply.github.com>
2022-05-20 16:20:04 +0000

    new update

--- script.ts
+++ script.ts
@@ -1,13 +1,52 @@
-const listener = Deno.listen({port: 8080});
+interface ServerConfig {
+    port: number;
+    addr: string;
+}
 
-const handle = async (connection: Deno.Conn) => {
-    for await (const { respondWith } of Deno.serveHttp(connection)) {
-        respondWith(new Response("Hello world From deno!"));
+type Router = () => Response;
+
+class HttpServer {
+    /**
+     * config object
+     */
+    config: ServerConfig;
+
+    /**
+     * Routers
+     */
+    routers: Router[];
+    
+    constructor(config: ServerConfig) {
+        this.config = config;
+        this.routers = [];
+    }
+
+    public route (router: Router): this {
+        this.routers.push(router);
+
+        return this;
     }
 }
 
+
+const listener = Deno.listen({port: 8080});
+
+// const handle = async (connection: Deno.Conn) => {
+//     for await (const { respondWith } of Deno.serveHttp(connection)) {
+//         respondWith(new Response("Hello world From deno!"));
+//     }
+// }
+
 (async () => {
-    for await (const conn of listener) {
-        handle(conn)
-    }
+    // for await (const conn of listener) {
+    //     handle(conn)
+    // }
+
+    const server = new HttpServer({
+        port: 8080,
+        addr: "123"
+    }).route(() => new Response("Hello world"));
+
+    console.log(server);
+    
 })()
